dee0b4b208692b2838abbe30b9a477e573b689fc
mdsfes <mds.skali@gmail.com>
2022-11-08 12:04:52 +0100

    calcule1

--- calculate/Form1.Designer.cs
+++ calculate/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.multiplaybtn = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.divise = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -64,6 +65,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -73,6 +75,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -82,6 +85,7 @@
             this.button4.TabIndex = 5;
             this.button4.Text = "6";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -91,6 +95,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -100,6 +105,7 @@
             this.button6.TabIndex = 3;
             this.button6.Text = "4";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -109,6 +115,7 @@
             this.button7.TabIndex = 8;
             this.button7.Text = "9";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -118,6 +125,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // btn7
             // 
@@ -127,6 +135,7 @@
             this.btn7.TabIndex = 6;
             this.btn7.Text = "7";
             this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
             // 
             // zero
             // 
@@ -136,6 +145,7 @@
             this.zero.TabIndex = 10;
             this.zero.Text = "0";
             this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
             // 
             // equalbtn
             // 
@@ -145,6 +155,7 @@
             this.equalbtn.TabIndex = 15;
             this.equalbtn.Text = "=";
             this.equalbtn.UseVisualStyleBackColor = true;
+            this.equalbtn.Click += new System.EventHandler(this.equalbtn_Click);
             // 
             // plus
             // 
@@ -154,6 +165,7 @@
             this.plus.TabIndex = 14;
             this.plus.Text = "+";
             this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
             // 
             // minus
             // 
@@ -163,6 +175,7 @@
             this.minus.TabIndex = 13;
             this.minus.Text = "-";
             this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
             // 
             // multiplaybtn
             // 
@@ -172,6 +185,7 @@
             this.multiplaybtn.TabIndex = 12;
             this.multiplaybtn.Text = "X";
             this.multiplaybtn.UseVisualStyleBackColor = true;
+            this.multiplaybtn.Click += new System.EventHandler(this.multiplaybtn_Click);
             // 
             // textBox1
             // 
@@ -189,6 +203,17 @@
             this.label1.Size = new System.Drawing.Size(301, 22);
             this.label1.TabIndex = 17;
             this.label1.Text = "Number";
+            this.label1.Click += new System.EventHandler(this.label1_Click_3);
+            // 
+            // divise
+            // 
+            this.divise.Location = new System.Drawing.Point(166, 265);
+            this.divise.Name = "divise";
+            this.divise.Size = new System.Drawing.Size(71, 65);
+            this.divise.TabIndex = 18;
+            this.divise.Text = "/";
+            this.divise.UseVisualStyleBackColor = true;
+            this.divise.Click += new System.EventHandler(this.divise_Click);
             // 
             // calculate
             // 
@@ -196,6 +221,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(325, 334);
+            this.Controls.Add(this.divise);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.equalbtn);
@@ -214,6 +240,7 @@
             this.Controls.Add(this.button1);
             this.Name = "calculate";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.calculate_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -237,6 +264,7 @@
         private System.Windows.Forms.Button multiplaybtn;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button divise;
     }
 }
 
--- calculate/Form1.cs
+++ calculate/Form1.cs
@@ -12,9 +12,17 @@ namespace calculate
 {
     public partial class calculate : Form
     {
+        int i;
+        string I;
+        int j;
+        int op;
         public calculate()
         {
             InitializeComponent();
+        }
+        private void Aff(int I)
+        {
+            this.textBox1.Text += ""+I;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -29,7 +37,7 @@ namespace calculate
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            Aff(1);
         }
 
         private void label1_Click_2(object sender, EventArgs e)
@@ -37,9 +45,118 @@ namespace calculate
 
         }
 
+        private void calculate_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Aff(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Aff(3);
+        }
+
+        private void multiplaybtn_Click(object sender, EventArgs e)
+        {
+            i *= int.Parse(this.textBox1.Text);
+            this.textBox1.Text = "";
+            op = 0;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Aff(4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Aff(5);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Aff(6);
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            i -= int.Parse(this.textBox1.Text);
+            this.textBox1.Text = "";
+            op = 1;
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            Aff(7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Aff(8);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Aff(9);
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            i += int.Parse(this.textBox1.Text);
+            this.textBox1.Text = "";
+            op = 2;
+            
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            Aff(0);
+        }
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void equalbtn_Click(object sender, EventArgs e)
+        {
+            j += int.Parse(this.textBox1.Text);
+            I += (i + j).ToString();
+            /*  if (op == 0)
+              {
+                  I += (i * j).ToString();
+              }
+              else if (op == 1)
+              {
+                  I += (i - j).ToString();
+              }
+              else if(op == 2)
+              {
+                  I += (i + j).ToString();
+              }
+            */
+            this.label1.Text = I;
+            i = 0;
+            j=0;
+            I = "";
+            this.textBox1.Text = "";
+            
+        }
+
+        private void divise_Click(object sender, EventArgs e)
+        {
+            i /= int.Parse(this.textBox1.Text);
+            this.textBox1.Text = "";
         }
     }
 }
